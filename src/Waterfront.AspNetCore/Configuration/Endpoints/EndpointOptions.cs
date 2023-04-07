1f81547eeacaef2f28772d72eee7f11109b1a267
2CHEVSKII <prsroman3@gmail.com>
2023-04-07 15:02:38 +0300

    Add project files

---
+++ src/Waterfront.AspNetCore/Configuration/Endpoints/EndpointOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Waterfront.AspNetCore.Configuration.Endpoints;
+
+public class EndpointOptions
+{
+    public PathString TokenEndpoint { get; set; }
+    public PathString InfoEndpoint { get; set; }
+    public PathString PublicKeyEndpoint { get; set; }
+}

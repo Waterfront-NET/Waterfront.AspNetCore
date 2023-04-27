5aa05e383e074723d14c4d129237857709ae358e
2CHEVSKII <prsroman3@gmail.com>
2023-04-27 15:14:27 +0300

    Small cleanup

--- src/Waterfront.AspNetCore/Services/TokenRequests/TokenRequestCreationService.cs
+++ src/Waterfront.AspNetCore/Services/TokenRequests/TokenRequestCreationService.cs
@@ -19,54 +19,43 @@ public class TokenRequestCreationService
 
     public TokenRequest CreateRequest(HttpContext context)
     {
-        _logger.LogDebug(
-            "Started binding context for request {@RequestId}",
-            context.TraceIdentifier
-        );
+        _logger.LogDebug("Started binding context for request {@RequestId}", context.TraceIdentifier);
 
         IQueryCollection query = context.Request.Query;
 
-        if ( !TryGetService(query, out string service) )
+        string service = query["service"].ToString();
+
+        if (string.IsNullOrEmpty(service))
         {
             _logger.LogError("Failed to get the 'service' value from request query");
             throw new InvalidOperationException("Failed to get value: service");
         }
 
-        string   account         = query["account"].ToString();
-        string   clientId        = query["client_id"].ToString();
-        string   strOfflineToken = query["offline_token"].ToString();
-        string[] strScopes       = query["scope"].ToArray();
+        string account = query["account"].ToString();
+        string clientId = query["client_id"].ToString();
+        string strOfflineToken = query["offline_token"].ToString();
+        string[] strScopes = query["scope"].ToArray();
 
-        BasicCredentials basicCredentials =
-        BasicAuthParser.IsBasicAuth(context.Request.Headers.Authorization) switch {
-            true  => BasicAuthParser.ParseHeaderValue(context.Request.Headers.Authorization),
-            false => new BasicCredentials { }
+        BasicCredentials basicCredentials = BasicAuthParser.IsBasicAuth(context.Request.Headers.Authorization) switch {
+            true => BasicAuthParser.ParseHeaderValue(context.Request.Headers.Authorization),
+            false => default(BasicCredentials)
         };
-        ConnectionCredentials   connectionCredentials   = context.GetConnectionCredentials();
-        RefreshTokenCredentials refreshTokenCredentials = new RefreshTokenCredentials { };
+        ConnectionCredentials connectionCredentials = context.GetConnectionCredentials();
+        RefreshTokenCredentials refreshTokenCredentials = new RefreshTokenCredentials {};
 
         return new TokenRequest {
-            Id      = context.TraceIdentifier,
+            Id = context.TraceIdentifier,
             Service = service,
             Account = account,
-            Client  = clientId,
+            Client = clientId,
             OfflineToken = strOfflineToken switch {
-                               { Length: not 0 } => bool.Parse(strOfflineToken),
-                               _                 => false
-                           },
-            Scopes = new List<TokenRequestScope>(
-                strScopes.Select(AclEntityParser.ParseTokenRequestScope)
-            ),
-            BasicCredentials        = basicCredentials,
-            ConnectionCredentials   = connectionCredentials,
+                {Length: not 0} => bool.Parse(strOfflineToken),
+                var _ => false
+            },
+            Scopes = new List<TokenRequestScope>(strScopes.Select(AclEntityParser.ParseTokenRequestScope)),
+            BasicCredentials = basicCredentials,
+            ConnectionCredentials = connectionCredentials,
             RefreshTokenCredentials = refreshTokenCredentials
         };
-    }
-
-    private bool TryGetService(IQueryCollection query, out string service)
-    {
-        service = query["service"].ToString();
-
-        return !string.IsNullOrEmpty(service);
     }
 }
